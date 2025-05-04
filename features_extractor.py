0f0ce23793b039b04db153c9c3cb37fe31ebd177
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2025-05-04 22:26:20 +0200

    Handling numpy_array and Image.Image in tensor creation.

--- features_extractor.py
+++ features_extractor.py
@@ -1,6 +1,8 @@
 import torch
 from torchvision import models, transforms, datasets
 from PIL import Image
+from typing import Union
+import numpy as np
 
 class ResNet50Extractor:
     IMAGENET_MEAN = models.ResNet50_Weights.DEFAULT.transforms().mean 
@@ -15,6 +17,7 @@ class ResNet50Extractor:
 
         self.feature_extractor.eval()
 
+    # Use to extract features from image tensor
     def extract(self, img_tensor: torch.Tensor) -> list[float]:
         features = None
         with torch.no_grad():
@@ -22,9 +25,31 @@ class ResNet50Extractor:
             features = features.view(features.size(0), -1) # Flatten
         return features[0]
     
+    # Use to create image tensor.
     @staticmethod
-    def get_image_tensor(img_path: str) -> torch.Tensor:
-        img = Image.open(img_path).convert('RGB')
+    def create_image_tensor(img_input: Union[str, Image.Image, np.ndarray]) -> torch.Tensor:
+        if isinstance(img_input, str):
+            img = Image.open(img_input)
+        elif isinstance(img_input, Image.Image):
+            img = img_input
+        elif isinstance(img_input, np.ndarray):
+            if img_input.ndim == 2:  # Grayscale
+                img = Image.fromarray(img_input).convert('RGB')
+            elif img_input.ndim == 3:
+                if img_input.shape[2] == 3:
+                    img = Image.fromarray(img_input.astype(np.uint8), 'RGB')
+                elif img_input.shape[2] == 4:
+                    img = Image.fromarray(img_input.astype(np.uint8), 'RGBA').convert('RGB')
+                else:
+                    raise ValueError("Unsupported channel number in numpy array.")
+            else:
+                raise ValueError("Unsupported numpy array shape.")
+        else:
+            raise TypeError("img_input must be a file path, PIL Image, or numpy array")
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
         transform = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
@@ -38,7 +63,8 @@ if __name__ == '__main__':
     extractor = ResNet50Extractor()
 
     for img_path in image_paths:
-        img = ResNet50Extractor.get_image_tensor(img_path)
+        img = ResNet50Extractor.create_image_tensor(img_path)
         features = extractor.extract(img)
-
-
+        print(len(features))
+        print(features)
+        print()
